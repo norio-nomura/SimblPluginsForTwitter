c2075016b9e236b3d5ab58ea8b2bf2a17161e41d
Norio Nomura <norio.nomura@gmail.com>
2014-02-14 20:20:16 +0900

    Add `MobypictureImageService`

---
+++ ExtendImageServiceForTwitter/MobypictureImageService.h
@@ -0,0 +1,12 @@
+//
+//  MobypictureImageService.h
+//  SimblPluginsForTwitter
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol ImageService;
+
+@interface MobypictureImageService : NSObject<ImageService>
+
+@end
